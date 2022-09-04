e63ce45c86eb5fe69f171da6be13b5dbfe8d7792
Ridwan Ridlo <murtiniibu110@gmail.com>
2022-09-04 14:28:33 +0700

    update limit top 10 movie

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
 {
     public function index () {
         return view ("index", [
-            "movies" => Movie::orderBy("rating", "desc")->paginate(10),
+            "movies" => Movie::orderBy("rating", "desc")->take(10)->get(),
             "genres" => Genre::get(),
             "title" => "Dashboard"
         ]);
